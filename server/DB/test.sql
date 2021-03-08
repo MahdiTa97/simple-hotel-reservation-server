063af9a63cf81f11f2e3872a8460b6d851f42453
Mahdi Taala <m.taala@tgbsco.local>
2021-03-08 14:02:32 +0330

    Improve payment table

--- server/DB/test.sql
+++ server/DB/test.sql
@@ -26,9 +26,9 @@ CREATE TABLE IF NOT EXISTS room
 (
     roomNo numeric(3) NOT NULL,
     hotelNo character varying(20) NOT NULL,
-    type character varying(10) NOT NULL,
-    price decimal(5,2),
-    status character varying(10) NOT NULL DEFAULT 'free',
+    roomType character varying(10) NOT NULL,
+    roomPrice decimal(5,2),
+    roomStatus character varying(10) NOT NULL DEFAULT 'free',
     PRIMARY KEY (roomNo, hotelNo),
     FOREIGN KEY (hotelNo)
         REFERENCES hotel (hotelNo) MATCH SIMPLE
@@ -41,7 +41,7 @@ WITH (
 );
 
 -- INSERT
-INSERT INTO room(roomNo, hotelNo, type, price, status)
+INSERT INTO room(roomNo, hotelNo, roomType, roomPrice, roomStatus)
 VALUES(501, 'fb01', 'single', 19, 'free'),
 (601, 'fb01', 'double', 29, 'free'),
 (701, 'fb01', 'family', 39, 'free'),
@@ -79,6 +79,29 @@ VALUES('0525551433', 'John' ,'Kay', '56 High St, London', '2001-10-05'),
 ('0020452934', 'Tina' ,'Murphy', '63 Well St, Glasgow', '2001-10-05'),
 ('0020452935', 'Tony' ,'Shaw', '12 Park Pl, Glasgow', '2001-10-05');
 
+-- -------------PAYMENT-------------
+CREATE TABLE IF NOT EXISTS payment
+(
+    paymentNo uuid NOT NULL DEFAULT uuid_generate_v4(),
+    paymentPrice decimal(5,2),
+    paymentType character varying(10) NOT NULL,
+    paymentDate date,
+    PRIMARY KEY (paymentNo)
+)
+WITH (
+    OIDS = FALSE
+);
+
+-- INSERT
+INSERT into payment(paymentNo, paymentPrice, paymentType, paymentDate)
+VALUES(uuid_generate_v4() ,55, 'cash', '2001-10-05'),
+(uuid_generate_v4() ,123, 'cash', '2001-10-05'),
+(uuid_generate_v4() ,12, 'cash', '2001-10-05'),
+(uuid_generate_v4() ,343, 'cash', '2001-10-05'),
+(uuid_generate_v4() ,23, 'cash', '2001-10-05'),
+(uuid_generate_v4() ,46, 'cash', '2001-10-05'),
+(uuid_generate_v4() ,57, 'cash', '2001-10-05');
+
 -- -------------BOOKING-------------
 CREATE TABLE IF NOT EXISTS booking
 (
@@ -89,7 +112,8 @@ CREATE TABLE IF NOT EXISTS booking
     roomNo numeric(3),
     paymentNo uuid,
     bookingStatus integer NOT NULL DEFAULT 0,
-    PRIMARY KEY (hotelNo,guestSsn,dateIn),
+    -- 0 iactive, 1 reserved but not paid, 2 reserved and paid
+    PRIMARY KEY (hotelNo, guestSsn, dateIn),
     FOREIGN KEY (roomNo, hotelNo)
         REFERENCES room (roomNo, hotelNo) MATCH SIMPLE
         ON UPDATE CASCADE
@@ -99,12 +123,12 @@ CREATE TABLE IF NOT EXISTS booking
         REFERENCES guest (guestSsn) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
-        NOT VALID,
-    FOREIGN KEY (paymentNo)
-        REFERENCES payment (paymentNo) MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
         NOT VALID
+    -- FOREIGN KEY (paymentNo)
+    --     REFERENCES payment (paymentNo) MATCH SIMPLE
+    --     ON UPDATE CASCADE
+    --     ON DELETE CASCADE
+    --     NOT VALID
 )
 WITH (
     OIDS = FALSE
@@ -112,26 +136,13 @@ WITH (
 
 -- INSERT
 INSERT into booking(hotelNo, guestSsn, dateIn, dateOut, roomNo, paymentNo, bookingStatus)
-VALUES("fb01", "0525551433" ,"2001-10-05", "2001-10-07", "501", null, "0"),
-("fb01", "0046525637" ,"2001-10-05", "2001-10-08", "601", null, "0"),
-("fb02", "0189973935" ,"2001-10-05", "2001-10-09", "111", null, "0"),
-("fb02", "0998525227" ,"2001-10-05", "2001-10-10", "101", null, "0"),
-("ch02", "0799624934" ,"2001-10-05", "2001-10-11", "701", null, "0"),
-("dc01", "0020452934" ,"2001-10-05", "2001-10-12", "901", null, "0"),
-("dc01", "0020452935" ,"2001-10-05", "2001-10-13", "111", null, "0");
-
--- -------------PAYMENT-------------
-CREATE TABLE IF NOT EXISTS payment
-(
-    "paymentNo" uuid NOT NULL DEFAULT uuid_generate_v4(),
-    "paymentPrice" character varying(250),
-    "type" character varying(250),
-    "date" timestamp NOT NULL DEFAULT NOW(),
-    PRIMARY KEY ("paymentNo")
-)
-WITH (
-    OIDS = FALSE
-);
+VALUES('fb01', '0525551433' ,'2001-10-05', '2001-10-07', 501, null, '0'),
+('fb01', '0046525637' ,'2001-10-05', '2001-10-08', 601, null, '0'),
+('fb02', '0189973935' ,'2001-10-05', '2001-10-09', 111, null, '0'),
+('fb02', '0998525227' ,'2001-10-05', '2001-10-10', 101, null, '0'),
+('ch02', '0799624934' ,'2001-10-05', '2001-10-11', 701, null, '0'),
+('dc01', '0020452934' ,'2001-10-05', '2001-10-12', 901, null, '0'),
+('dc01', '0020452935' ,'2001-10-05', '2001-10-13', 111, null, '0');
 
 -- -------------PERSONNEL-------------
 CREATE TABLE IF NOT EXISTS personnel
